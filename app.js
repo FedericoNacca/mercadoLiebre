f918ed53d4b54dbc1c3cabaee678cf28783a6dcd
FedericoNacca <t4k490@gmail.com>
2024-02-27 13:02:56 -0300

    fix: modificando app.js

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const publicPath = path.resolve(__dirname, './public');
 
-app.use(express.static(publicPath));
+app.use(express.static("public"));
 
-app.listen(3000, () => {
-    console.log('servidor corriendo en el puerto 3000');
+app.get("/", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./view/home.html"));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+app.get("/login.html", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./view/login.html"));
 });
 
-app.get("*", (req, res) => {
-    res.status(404).send("404 not found. <br> !Huston estamos en problemas!");
+app.post("/login.html", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./view/home.html"));
 });
 
+app.get("/registro.html", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./view/registro.html"));
+});
+
+app.post("/registro.html", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./view/home.html"));
+});
+
+app.listen(3000, () => console.log("Servidor corriendo"));
